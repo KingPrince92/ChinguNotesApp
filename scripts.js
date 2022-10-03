394881f1b1122c63a46c8d22d620c68bdfa3aa0a
Milo <miloprince92@gmail.com>
2022-10-03 12:02:38 -0400

    Rework - new note and edit work. Light styling

--- scripts.js
+++ scripts.js
@@ -1,104 +1,51 @@
-const userNotes = [
-  {
-    title: "My Very First Note",
-    notetext: "it's incredible!",
-  },
-  {
-    title: "my second note",
-    notetext: "feed my dogs",
-  },
-];
+const addButton = document.getElementById("add");
+const notes = JSON.parse(localStorage.getItem("notes"));
+const mainBody = document.getElementById("main");
 
-const notesContainer = document.querySelector(".notes-container");
-const notesForm = document.querySelector(".notes-form");
-const modal = document.querySelector(".modal");
-const editText = document.getElementById("#edittext");
-const edittitle = document.getElementById("#edittitle");
-const editButton = document.querySelector("submitEdit");
+const updateLocalStorage = () => {
+  const notesText = document.querySelectorAll("textarea");
+  const notes = [];
+  notesText.forEach((note) => notes.push(note.value));
+  localStorage.setItem("notes", JSON.stringify(notes));
+};
 
-const createNote = () => {
-  notesContainer.innerHTML = "";
-  userNotes.forEach((note, i) => {
-    const newNote = document.createElement("div");
-    newNote.classList.add("note");
-    const noteTitle = document.createElement("div");
-    const notesContent = document.createElement("div");
-    const trash = document.createElement("button");
-    const edit = document.createElement("button");
-    trash.classList.add("fa-solid", "fa-trash");
-    edit.classList.add("fa-solid", "fa-pen");
-    trash.setAttribute("data-index", i);
-    edit.setAttribute("data-index", i);
-    noteTitle.textContent = note.title;
-    notesContent.textContent = note.notetext;
-    newNote.append(noteTitle, notesContent, trash, edit);
-    notesContainer.append(newNote);
+const addNewNote = (text = "") => {
+  const note = document.createElement("div");
+  note.classList.add("note");
+  note.innerHTML = `
+  <div class="tools">
+        <button class="edit"><i class="fas fa-edit"></i></button>
+        <button class="delete"><i class="fas fa-trash-alt"></i></button>
+  </div>
+  <div class="main ${text ? "" : "hidden"}"></div>
+  <textarea class="${text ? "hidden" : ""}"></textarea>`;
+
+  const editButton = note.querySelector(".edit");
+  const deleteButton = note.querySelector(".delete");
+  const main = note.querySelector(".main");
+  const textArea = note.querySelector("textarea");
+  textArea.value = text;
+
+  deleteButton.addEventListener("click", () => {
+    note.remove();
+    updateLocalStorage();
   });
+  editButton.addEventListener("click", () => {
+    main.classList.toggle("hidden");
+    textArea.classList.toggle("hidden");
+  });
+  textArea.addEventListener("keypress", (e) => {
+    if (e.key === "Enter") {
+      const { value } = e.target;
+      main.innerHTML = value;
+      updateLocalStorage();
+    }
+  });
+  mainBody.appendChild(note);
 };
 
-notesForm.addEventListener("submit", (event) => {
-  event.preventDefault();
-  const title = document.querySelector("#title").value;
-  const notetext = document.querySelector("#note-input").value;
-  const newNote = {
-    title,
-    notetext,
-  };
-  userNotes.push(newNote);
-  createNote();
-});
+addButton.addEventListener("click", () => addNewNote());
 
-const toggleHide = () => {
-  modal.classList.toggle("hide");
-};
-
-//removing a note
-notesContainer.addEventListener("click", (e) => {
-  if (e.target.classList.contains("fa-trash")) {
-    const index = e.target.getAttribute("data-index");
-    userNotes.splice(index, 1);
-    createNote();
-  }
-});
-
-//editing text
-notesContainer.addEventListener("click", (e) => {
-  if (e.target.classList.contains("fa-pen")) {
-    const index = e.target.getAttribute("data-index");
-    toggleHide();
-    editNote(index);
-  }
-});
-
-const editNote = (cardIndex) => {
-  const title = document.querySelector("#edittitle").value;
-  const notetext = document.querySelector("#edittext").value;
-  note[cardIndex].title = title;
-  note[cardIndex].notetext = notetext;
-  createNote();
-};
-
-editButton.addEventListener("click", editNote);
-
-createNote();
-
-// notesContainer.addEventListener("click", (e) => {
-//   if (e.target.classList.contains("fa-pen")) {
-//     const changingNote = e.target.parentElement;
-//     console.log(e.target.parentElement);
-//     toggleHide();
-//   }
-// });
-
-// //editing text
-// editButton.addEventListener("click", (e) => {
-//   e.preventDefault();
-//   const title = document.querySelector("#edittitle").value;
-//   const notetext = document.querySelector("#edittext").value;
-//   openEdit[0].title = title;
-//   openEdit[0].notetext = notetext;
-//   toggleHide();
-//   createNote();
-//   openEdit = [];
-// });
-// createNote();
+if (notes) {
+  notes.forEach((note) => addNewNote(note));
+}
